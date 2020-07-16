07f5d63a6c94adfe156fd2bd7780272e9e00894b
nakenapalli <33691732+nakenapalli@users.noreply.github.com>
2020-07-16 14:07:06 -0400

    first commit

---
+++ lib/contact_model.dart
@@ -0,0 +1,17 @@
+class Contact {
+  final int id;
+  final String name;
+  final String phone;
+  final String email;
+  final String type;
+  final String country;
+
+  Contact({
+    this.id,
+    this.name,
+    this.phone,
+    this.email,
+    this.type,
+    this.country,
+  });
+}
