730904179465510ac4d0db8b1e125ccfa0f0a4dc
nakenapalli <33691732+nakenapalli@users.noreply.github.com>
2020-07-23 08:55:41 -0400

    created basic contact form page

---
+++ lib/contact_form.dart
@@ -0,0 +1,43 @@
+import "package:flutter/material.dart";
+
+class ContactForm extends StatefulWidget {
+  ContactForm({Key key}) : super(key: key);
+
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contact Name"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            RaisedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  print("Entries are valid");
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
